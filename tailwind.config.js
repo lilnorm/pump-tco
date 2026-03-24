38a009eaebf7423cd8d24aa1623484d40960f406
Naphon Seeluang <lotta.comma@gmail.com>
2026-03-24 13:13:58 +0700

    fix design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // บรรทัดนี้สำคัญมาก ต้องมี!
   ],
   theme: {
     extend: {},
